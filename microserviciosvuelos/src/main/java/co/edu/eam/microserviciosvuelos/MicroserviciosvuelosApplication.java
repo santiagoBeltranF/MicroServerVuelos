9a896805beb2001a63c48e1c152dec8c773c8286
santiagobeltranf <santiago.beltran.7813@eam.edu.co>
2024-04-15 18:51:48 -0500

    Server Vuelos

---
+++ microserviciosvuelos/src/main/java/co/edu/eam/microserviciosvuelos/MicroserviciosvuelosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eam.microserviciosvuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosvuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosvuelosApplication.class, args);
+	}
+
+}
---
+++ microserviciosvuelos/src/test/java/co/edu/eam/microserviciosvuelos/MicroserviciosvuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eam.microserviciosvuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosvuelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
